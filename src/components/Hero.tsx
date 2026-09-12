2f2ad2c2c420ffea5f673da016a69d2ef9e54790
mdasaduzzamanminhaz3 <mdasaduzzamanminhaz3@gmail.com>
2026-09-13 02:01:24 +0600

    hero section implemented

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-
+import HeroImg from "../assets/banner-stack.png";
 const Hero = () => {
-    return (
-        <div>
-            
+  return (
+    <section className="container mx-auto">
+      <div className="flex items-center justify-between px-4 py-20">
+        <div className="flex flex-col w-1/2">
+          <h1 className="text-4xl font-bold">
+            Build Your Ideal <br /><span className="bg-gradient-to-r from-orange-600 to-purple-600 bg-clip-text text-transparent">Development Stack </span>
+          </h1>
+          <span className="text-gray-700 mt-4 text-start"> 
+            Explore frontend, backend, database, and tooling options, compare
+            them side by side, and put together the stack that fits your next
+            project.
+          </span>
+
+          <div className="flex gap-4 mt-8 text-start items-center">
+          <button className="btn rounded-md text-white bg-gradient-to-r from-orange-500 to-pink-600">Explore Technologies</button>
+          <button className="btn">Learn More</button>
+          </div>
         </div>
-    );
+        <div>
+          <img src={HeroImg} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
