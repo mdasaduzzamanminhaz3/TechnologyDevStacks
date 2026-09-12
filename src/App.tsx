2b5b28e96cd7a16a4f49b626753cc1e2848ff2d8
mdasaduzzamanminhaz3 <mdasaduzzamanminhaz3@gmail.com>
2026-09-13 02:00:09 +0600

    navbar implemented

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
 
   return (
     <>
-    hello
     <NavBar/>
     <Hero/>
     <TStacks/>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-
+import NavLogo from "../assets/logo-text.png"
 const NavBar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className="container mx-auto px-6 py-3 bg-gray-50 sticky">
+            <div className="flex justify-between items-center">
+                <div><img src={NavLogo} alt="" /></div>
+                <ul className="flex gap-7 text-gray-700">
+                    <li className="hover:text-pink-600 cursor-pointer">Home</li>
+                    <li className="hover:text-pink-600 cursor-pointer">Technologies</li>
+                    <li className="hover:text-pink-600 cursor-pointer">Projects</li>
+                    <li className="hover:text-pink-600 cursor-pointer">About</li>
+                    <li className="hover:text-pink-600 cursor-pointer">Contact</li>
+                </ul>
+                <div className="flex gap-4">
+                    <button className="btn rounded-3xl hover:bg-pink-600 hover:text-white">Sign In</button>
+                    <button className="btn rounded-3xl bg-pink-600 text-white hover:bg-pink-700">Sign Up</button>
+                </div>
+            </div>
+        </nav>
     );
 };
 
 export default NavBar;
+
